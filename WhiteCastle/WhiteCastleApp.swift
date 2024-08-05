64f18f65cd58263eb57ae2af3e6606e8aa9f6feb
ich <git@of15-15.de>
2024-08-05 21:53:20 +0200

    Initial Commit

---
+++ WhiteCastle/WhiteCastleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteCastleApp.swift
+//  WhiteCastle
+//
+//  Created by Ich on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct WhiteCastleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhiteCastleTests/WhiteCastleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteCastleTests.swift
+//  WhiteCastleTests
+//
+//  Created by Ich on 05.08.24.
+//
+
+import Testing
+@testable import WhiteCastle
+
+struct WhiteCastleTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
