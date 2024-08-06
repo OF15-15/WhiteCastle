871b6962884d24e0513c1165e6974ede723b1ac0
ich <git@of15-15.de>
2024-08-06 13:09:27 +0200

    implemented player board actions

--- WhiteCastle/Action.swift
+++ WhiteCastle/Action.swift
@@ -43,3 +43,70 @@ class WellAction: Action {
         return true
     }
 }
+
+class PlayerBoardAction: Action {
+    convenience init(diceColor dCol: DColor) {
+        self.init(diceColor: dCol, actionValue: 6)
+    }
+    override init(diceColor dCol: DColor, actionValue actVal: Int) {
+        super.init(diceColor: dCol, actionValue: actVal)
+    }
+    
+    override func run(player: Player, gameBoard: GameBoard, diceValue: Int) -> Bool {
+        if !super.run(player: player, gameBoard: gameBoard, diceValue: diceValue) {
+            return false
+        }
+        
+        switch self.diceColor {
+        case .red:
+            let bonuses = 1...(5-player.courtiers)
+            for bonus in bonuses {
+                switch bonus {
+                case 0, 1, 2, 4:
+                    player.gainFood(1)
+                case 3:
+                    player.gainCoins(2)
+                case 5:
+                    player.gainSeals(1)
+                default:
+                    raise(43)
+                }
+            }
+            //TBD: player.actionCard.black.run()
+        case .black:
+            let bonuses = 1...(5-player.gardeners)
+            for bonus in bonuses {
+                switch bonus {
+                case 0, 1, 2, 4:
+                    player.gainIron(1)
+                case 3:
+                    player.lantern.run()
+                case 5:
+                    player.gainCoins(2)
+                default:
+                    raise(43)
+                }
+            }
+            //TBD: player.actionCard.black.run()
+            
+        case .white:
+            let bonuses = 1...(5-player.warriors)
+            for bonus in bonuses {
+                switch bonus {
+                case 0, 1, 2, 4:
+                    player.gainPearls(1)
+                case 3:
+                    player.gainSeals(1)
+                case 5:
+                    player.lantern.run()
+                default:
+                    raise(43)
+                }
+            }
+            //TBD: player.actionCard.black.run()
+        case .all:
+            raise(42)
+        }
+    }
+}
+
