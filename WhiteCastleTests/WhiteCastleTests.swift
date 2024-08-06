63c140dda96da3df8aeaf814228c31d885acb833
ich <git@of15-15.de>
2024-08-06 15:07:10 +0200

    added tests for gaining new action cards and the correct lantern resolution afterwards

--- WhiteCastleTests/WhiteCastleTests.swift
+++ WhiteCastleTests/WhiteCastleTests.swift
@@ -9,9 +9,35 @@ import Testing
 @testable import WhiteCastle
 
 struct WhiteCastleTests {
-
-    @Test func testExample() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @Test testLanternCards() {
+        
+        let src = StartResourceCard(lanternResource: .food, food: 2, iron: 1, pearls: 3, coins: 2, seals: 1)
+        let sac = StartActionCard(action: Action())
+        let p = Player(startResourceCard: src, startActionCard: sac)
+        
+        #expect(p.lanternCards[0] === src)
+        p.lantern.run(player: p)
+        #expect(p.food == 3)
+                
+        p.gainActionCard(FirstLevelCard(top: Action(), middle: Action(), bottom: Action()))
+        p.lantern.run(player: p)
+        #expect(p.food == 4)
+        #expect(p.influence == 1)
+        
+        p.gainActionCard(SecondLevelCard(top: Action(), middle: Action(), bottom: Action()))
+        p.lantern.run(player: p)
+        #expect(p.food == 5)
+        #expect(p.influence == 2)
+        #expect(p.coins == 3)
+        
+        p.gainActionCard(SecondLevelCard(top: Action(), middle: Action(), bottom: Action()))
+        p.lantern.run(player: p)
+        #expect(p.food == 6)
+        #expect(p.influence == 3)
+        #expect(p.coins == 4)
+        #expect(p.points == 1)
+        
+        
     }
-
 }
