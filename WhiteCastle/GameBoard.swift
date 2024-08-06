2ff515491ce918fe6683c747272d356f3b5440a9
ich <git@of15-15.de>
2024-08-06 15:38:35 +0200

    setup from game rules

--- WhiteCastle/GameBoard.swift
+++ WhiteCastle/GameBoard.swift
@@ -18,4 +18,29 @@ struct GameBoard {
     let castleThird: CastleTop
     
     let trainingYard5, trainingYard3, trainingYard1: TrainingYard
+    
+    
+    init() {
+        // setup guide
+        
+        // shuffle first / second level cards
+        // put a card in each of the open slots
+        // top castle card
+        
+        // all three colors in top spot, first level
+        // in number order: rest of dice tiles
+        // 6-10 if all tiles same color -> tile into next available space
+        // well tiles
+        
+        // shuffle plant / stone stack, one card onto each spot
+        
+        // 4 randomly selected yard tiles
+        
+        // bridges / rounds / players
+        
+        // turn order
+        // random src (p+1) add sac below each
+        // in opposite order: choos one pair
+        // decree card
+    }
 }
