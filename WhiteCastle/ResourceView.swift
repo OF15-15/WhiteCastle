9faea920e48f9802169a634d985f813715bf59d4
ich <git@of15-15.de>
2024-08-07 11:22:13 +0200

    PlayView

---
+++ WhiteCastle/ResourceView.swift
@@ -0,0 +1,18 @@
+//
+//  ResourceView.swift
+//  WhiteCastle
+//
+//  Created by Ich on 07.08.24.
+//
+
+import SwiftUI
+
+struct ResourceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResourceView()
+}
