28ba22e373d56c2a760916997dc76e3162ca247a
ich <git@of15-15.de>
2024-08-09 21:31:11 +0200

    resourceView

--- WhiteCastle/ResourceView.swift
+++ WhiteCastle/ResourceView.swift
@@ -9,11 +9,47 @@ import SwiftUI
 
 struct ResourceView: View {
     var body: some View {
+        let gb = GameBoard()
+        let p = gb.players[0]
         HStack {
             VStack {
+                Image("food")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text(String(p.food))
+                    .font(.largeTitle)
+            }
+            VStack {
+                Image("iron")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                 
+                Text(String(p.iron))
+                    .font(.largeTitle)
+            }
+            VStack {
+                Image("pearls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text(String(p.pearls))
+                    .font(.largeTitle)
+            }
+            VStack {
+                Image("coins")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text(String(p.coins))
+                    .font(.largeTitle)
+            }
+            VStack {
+                Image("seals")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text(String(p.seals))
+                    .font(.largeTitle)
             }
         }
+        .padding()
     }
 }
 
